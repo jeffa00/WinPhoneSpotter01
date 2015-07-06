502e204ff740ede2bc3283a6799b9cfa40a05413
jeffa <jeffa00@gmail.com>
2015-07-05 22:19:50 -0400

    Cleaned up some dev specific comments, etc.

--- src/WebApplication3/Controllers/PhoneSpottingController.cs
+++ src/WebApplication3/Controllers/PhoneSpottingController.cs
@@ -43,11 +43,6 @@ namespace WebApplication3.Controllers
                 return RedirectToAction("Index");
             }
 
-            var errors = ModelState
-                .Where(x => x.Value.Errors.Count > 0)
-                .Select(x => new { x.Key, x.Value.Errors })
-                .ToArray();
-
             return View(newSpotting);
         }
 
--- src/WebApplication3/wwwroot/scripts/phoneSpotter.js
+++ src/WebApplication3/wwwroot/scripts/phoneSpotter.js
@@ -1,14 +1,11 @@
 ﻿$(document).ready(function(){
     if("geolocation" in navigator){
-        //alert("Geolocate found.");
         navigator.geolocation.getCurrentPosition(function(position){
         $("#Longitude").val(position.coords.longitude);
         $("#Latitude").val(position.coords.latitude);
         
         var currentTime = new Date();
-        var currentTimeDisplay = currentTime.toLocaleString();
-        
-        $('#SpotTime').val(currentTimeDisplay);
+        $('#SpotTime').val(currentTime.toLocaleString());
         });
     } else {
         alert("Geolocate NOT found.");
